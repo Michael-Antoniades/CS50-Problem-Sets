5996b882346bb5a9d2bd1a4e2e67678c69d69e3f
bobboy65 <scmikeyt@yahoo.com>
2021-08-30 16:45:44 -0400

    Update helpers.c

--- pset4/helpers.c
+++ pset4/helpers.c
@@ -1,5 +1,7 @@
 #include "helpers.h"
 #include <math.h>
+#include <stdio.h>
+#include <cs50.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -20,7 +22,6 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 
         }
     }
-    return;
 }
 
 int limit(int RGB)
@@ -35,13 +36,42 @@ int limit(int RGB)
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+     int temp[3];
+     for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width/2; j++)
+        {
+            temp[0] =image[i][j].rgbtRed;
+            temp[1] =image[i][j].rgbtBlue;
+            temp[2] =image[i][j].rgbtGreen;
+
+
+            image[i][j].rgbtRed = image[i][width - 1 - j ].rgbtRed;
+            image[i][j].rgbtBlue = image[i][width - 1 - j ].rgbtBlue;
+            image[i][j].rgbtGreen = image[i][width - 1 - j ].rgbtGreen;
+
+            image[i][width-j-1].rgbtRed = temp[0];
+            image[i][width-j-1].rgbtBlue = temp[1];
+            image[i][width-j-1].rgbtGreen = temp[2];
+        }
+    }
+
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+        float average;
+
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+
+
+
+        }
+    }
 }
 
 // Detect edges
@@ -49,4 +79,3 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
 {
     return;
 }
-
