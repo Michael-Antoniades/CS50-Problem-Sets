dd348ca2d87cbc41cc51d737f1d2a0c8731aa59c
bobboy65 <scmikeyt@yahoo.com>
2021-08-30 15:32:22 -0400

    Update helpers.c

--- pset4/helpers.c
+++ pset4/helpers.c
@@ -1,1 +1,52 @@
+#include "helpers.h"
+#include <math.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    float average;
+
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+
+             //grayscale type declaration
+            average = round( (image[j][i].rgbtRed + image[j][i].rgbtGreen + image[j][i].rgbtBlue) / (3.000));
+
+            image[j][i].rgbtRed = average;
+            image[j][i].rgbtBlue = average;
+            image[j][i].rgbtGreen = average;
+
+        }
+    }
+    return;
+}
+
+int limit(int RGB)
+{
+    if (RGB > 255)
+    {
+        RGB = 255;
+    }
+    return RGB;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
 
