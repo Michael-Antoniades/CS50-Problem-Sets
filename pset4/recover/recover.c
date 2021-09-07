4d7dad65d17e2e5ed016ee0502d2d3dcad8e34dc
bobboy65 <scmikeyt@yahoo.com>
2021-09-07 16:47:59 -0400

    Update recover.c

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <getopt.h> 
+ 
  
 int main(int argc, char *argv[])
 {
+ if (argc == 1) {
+     //main code
+     typedef uint8_t BYTE; 
+     
+     
+     FILE *file = fopen(argv[1], "r");
+     
+     fread(char *buffer[], BYTE ,512, file* )
+     
+     
+ }
  
+ else if (argc != 1) {
+     return 1;
+     fprintf("Only one argument at a time can be passed\n");
+ }
 }
