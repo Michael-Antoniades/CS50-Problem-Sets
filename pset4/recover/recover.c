86ee9417204f299d8824eef311c7065e353a1e2a
bobboy65 <scmikeyt@yahoo.com>
2021-09-08 16:47:44 -0400

    Update recover.c

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,25 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <getopt.h> 
- 
- 
+#include <getopt.h>
+
 int main(int argc, char *argv[])
+
 {
- if (argc == 1) {
+     //argc has 2 calls, one by default and one to the program itself
      //main code
-     typedef uint8_t BYTE; 
-     
-     
-     FILE *file = fopen(argv[1], "r");
-     
-     fread(char *buffer[], BYTE ,512, file* )
-     
-     
- }
- 
- else if (argc != 1) {
+
+     if (argc != 2) {
+     printf("Usage: ./recover card.raw");
      return 1;
-     fprintf("Only one argument at a time can be passed\n");
- }
+     }
+
+
+
+     FILE *file = fopen(argv[1], "r");
+
+
+     if (file == NULL)
+     {
+     printf("Error: cannot open file\n");
+     return 2;
+     }
+
+     unsigned char buffer[512];
+
+
+     //will count how many images
+     int count = 0;
+
+     FILE *jpeg = NULL; //file we will be writing to
+
+     char *filename = malloc(8 * sizeof(char));
+     //char filename[8];
+
+     while (fread(buffer, sizeof(char) ,512, file))
+     {
+
+      if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0) {
+
+       sprintf(filename, "%03i.jpg", count);
+
+       jpeg = fopen(filename,"w");
+
+       count++;
+      }
+
+      if (jpeg != NULL) {
+      fwrite(&buffer, sizeof(char), 512, jpeg);
+      }
+
+      }
+
+
+ free(filename);
+ fclose(file);
+ fclose(jpeg);
+
+ return 0;
+
 }
