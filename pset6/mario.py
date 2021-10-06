7b00efa9729274917e77231446f99bb95bbf4ee6
bobboy65 <scmikeyt@yahoo.com>
2021-10-06 10:56:59 -0400

    Update mario.py

--- pset6/mario.py
+++ pset6/mario.py
@@ -1,6 +1,10 @@
 from cs50 import get_int
 
-n = get_int("Height: ")
+while True:
+    n = get_int("Height: ")
+    
+    if n >=0 and n <=23:
+        break
 
 for i in range(n):
     for j in range(n-1, i ,-1):
@@ -8,8 +12,7 @@ for i in range(n):
     for j in range(0,i+1,1):
         print("#", end = "")
      
-    print(" " + "#"*(j+1))    #crazy cool line of code in PY that took a minute to discover to replace our 3rd for loop from c mario problem
+    print("  " + "#"*(j+1))    #crazy cool line of code in PY that took a minute to discover to replace our 3rd for loop from c mario problem
         
 
    
-    
