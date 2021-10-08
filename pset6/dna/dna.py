50cd8e411177dabb139e8dc9d6ed5621106abcc1
bobboy65 <scmikeyt@yahoo.com>
2021-10-08 16:41:27 -0400

    Update dna.py

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -9,15 +9,13 @@ def main():
         
         with open(argv[1] , "r" ) as file:
             reader = csv.reader(file)
-            
-            for line in reader:
-                name = line[0]
-                counts = line
-                dictionary = {
-                    name : counts[1:len(line)]
-                }
+  
+            dict_from_csv = {line[0]:line[1:len(line)] for line in reader} #very powerful line of code 
+            # I had a for loop seperate to start for line in reader:, and could successfully loop through CSV
+            # but was having trouble assigning it to an array, was able to make this dynamic array with csv data AND 
+            # have for loop within it.  Found this helpful reference: https://www.delftstack.com/howto/python/python-csv-to-dictionary/
                 
-                print(dictionary)
+              
                 
         with open(argv[2] , "r") as file2:
             reader = csv.reader(file2)
@@ -25,8 +23,23 @@ def main():
             for array in reader:
                 sequence = array
                 
+        loop_length = 0 
+        loop_length = len(dict_from_csv)       
+        print(loop_length)
+        print(dict_from_csv)
         
-        for         
+        
+        #key,val = dict_from_csv.items()[0]
+        #for value in dict_from_csv.values():
+         #   if value =
+        #sequence = dict_from_csv.get(0)
+        #print(sequence)
+        for key, value in dict_from_csv.items():
+            print(key,value)
+            if key == "name":
+                break
+        
+            
             
                 
                 
@@ -41,7 +54,8 @@ def main():
     
     
     
-    elif len(argv) != 3:               #moved this non-3 argv case at the bottom of main to keep it out of the way 
+    elif len(argv) != 3:               
+        #moved this non-3 argv case at the bottom of main to keep it out of the way 
         print("Proper usage is: python dna.py data.csv sequence.txt")
         #sys.exit(1)
 if __name__ == "__main__":
