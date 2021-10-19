fac97fb1503a8b9bb45941016ed27009b7194821
bobboy65 <scmikeyt@yahoo.com>
2021-10-19 12:24:00 -0400

    Create 11.sql

---
+++ pset7/11.sql
@@ -0,0 +1,9 @@
+SELECT title
+FROM movies
+JOIN stars ON  movies.id = stars.movie_id
+JOIN ratings ON stars.movie_id = ratings.movie_id
+JOIN people ON stars.person_id = people.id
+WHERE name
+LIKE "Chadwick Boseman%"
+ORDER BY rating
+DESC LIMIT 5;
